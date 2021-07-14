4c70c0bbd818510243b1700eef6d71a486b558a1
Gabriel LaBrie <gabriel.labrie.professional@gmail.com>
2021-07-13 20:08:07 -0600

    Created component files inside source and rendered navbar component through app

--- cryptotrader/src/App.js
+++ cryptotrader/src/App.js
@@ -1,10 +1,13 @@
-
+import React from 'react'
+import Navbar from './component/navbar/Navbar'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
           CryptoTrader
+          <Navbar/>
 
     </div>
   );
