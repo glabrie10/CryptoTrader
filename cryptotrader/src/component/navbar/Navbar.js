d60fdb8367215097a33e7322a4b7a9532d8d893e
Gabriel LaBrie <gabriel.labrie.professional@gmail.com>
2021-07-14 14:31:51 -0600

    CREATED NAVBAR
    Added home, coin, contact and about links in a navbar.  Used traditional HTML and CSS.

--- cryptotrader/src/component/navbar/Navbar.js
+++ cryptotrader/src/component/navbar/Navbar.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import './Navbar.css';
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-       navbar
-      </div>
+       <header>
+         <div className="container">
+           <h3 className="logo">
+             <a href="#">CryptoTrader</a>
+           </h3>
+           <nav>
+             <li><a href="#">home</a></li>
+             <li><a href="#">coins</a></li>
+             <li><a href="#">contact</a></li>
+             <li><a href="#">about</a></li>
+
+           </nav>
+
+
+         </div>
+       </header>
     )
   }
 }
