684549139c29a89bf6c8ab23473d7a6d23f2553d
Gabriel LaBrie <gabriel.labrie.professional@gmail.com>
2021-07-26 10:07:16 -0600

    ADDED INTERNET EXPLORER COMPATIBILITY Added internet explorer compativility to index.html

--- cryptotrader/src/App.js
+++ cryptotrader/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './component/navbar/Navbar'
+import Home from './component/home/Home'
 import './App.css';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
           <Navbar/>
+          <Home/>
 
     </div>
   );
---
+++ cryptotrader/src/component/home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Image from '../../img/crypto.jpg'
+import './Home.css'
+
+export default class Home extends Component {
+  render () {
+    return (
+
+
+        <img className="image" src={Image} alt="crypto"/>
+
+
+    )
+  }
+}
